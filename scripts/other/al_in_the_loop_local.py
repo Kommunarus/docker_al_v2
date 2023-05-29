fa2f5bae229f5ae0b66586743898ba39bde55c46
alex <kooptsov@gmail.com>
2023-05-29 15:45:58 +0300

    local exp + dvc

--- scripts/other/al_in_the_loop_local.py
+++ scripts/other/al_in_the_loop_local.py
@@ -11,34 +11,8 @@ import datetime
 from dvclive import Live
 
 
-def al(live, add, path_model=''):
+def al(params):
     url = 'http://127.0.0.1:5000/active_learning'
-    params = {
-        'gpu': '0',
-        'path_to_img_train': '/media/alex/DAtA4/Datasets/coco/my_dataset/train',
-        'path_to_labels_train': '/media/alex/DAtA4/Datasets/coco/for_al',
-        'path_to_img_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/val',
-        'path_to_labels_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/labels_val/val.json',
-        'pretrain_from_hub': True,
-        'save_model': False,
-        'path_model': path_model,
-        'retrain_user_model': False,
-        'add': add,
-        'batch_unlabeled': -1,
-        'use_val_test_in_train': True,
-        'bbox_selection_policy': 'min',
-        'quantile_min': 0,
-        'quantile_max': 0.3,
-        'type_model': 'custom',
-        'num_epochs': 5
-    }
-
-    live.log_param("type_model (al)", params['type_model'])
-    live.log_param("quantile_min (al)", params['quantile_min'])
-    live.log_param("quantile_max (al)", params['quantile_max'])
-    live.log_param("add (al)", params['add'])
-    live.log_param("batch_unlabeled (al)", params['batch_unlabeled'])
-    live.log_param("bbox_selection_policy (al)", params['bbox_selection_policy'])
     # querystring = parse.urlencode(params)
     #
     # u = request.urlopen(url + '?' + querystring)
@@ -47,28 +21,9 @@ def al(live, add, path_model=''):
     out = train_api(**params)
     return out
 
-def mAP(live):
+def mAP(params):
     url = 'http://127.0.0.1:5000/eval'
-    params = {
-        'gpu': '0',
-        'path_to_img_train': '/media/alex/DAtA4/Datasets/coco/my_dataset/train',
-        'path_to_labels_train': '/media/alex/DAtA4/Datasets/coco/for_al',
-        'path_to_img_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/val',
-        'path_to_labels_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/labels_val/val.json',
-        'path_to_img_test': '/media/alex/DAtA4/Datasets/coco/my_dataset/test',
-        'path_to_labels_test': '/media/alex/DAtA4/Datasets/coco/my_dataset/labels_test/test.json',
-        'pretrain_from_hub': True,
-        'save_model': True,
-        'path_model': '',
-        'retrain_user_model': False,
-        'type_model': 'custom',
-        'num_epochs': 5
 
-    }
-    live.log_param("type_model (val)", params['type_model'])
-    live.log_param("num_epochs (val)", params['num_epochs'])
-    live.log_param("pretrain_from_hub (val)", params['pretrain_from_hub'])
-    live.log_param("retrain_user_model (val)", params['retrain_user_model'])
 
     # querystring = parse.urlencode(params)
     #
@@ -80,6 +35,52 @@ def mAP(live):
 
 if __name__ == '__main__':
     with Live(dir='experiment_al_detect' ,save_dvc_exp=True) as live:
+        params_map = {
+            'gpu': '0',
+            'path_to_img_train': '/media/alex/DAtA4/Datasets/coco/my_dataset/train',
+            'path_to_labels_train': '/media/alex/DAtA4/Datasets/coco/for_al',
+            'path_to_img_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/val',
+            'path_to_labels_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/labels_val/val.json',
+            'path_to_img_test': '/media/alex/DAtA4/Datasets/coco/my_dataset/test',
+            'path_to_labels_test': '/media/alex/DAtA4/Datasets/coco/my_dataset/labels_test/test.json',
+            'pretrain_from_hub': True,
+            'save_model': True,
+            'path_model': '',
+            'retrain_user_model': False,
+            'type_model': 'custom',
+            'num_epochs': 5
+
+        }
+        live.log_param("type_model (val)", params_map['type_model'])
+        live.log_param("num_epochs (val)", params_map['num_epochs'])
+        live.log_param("pretrain_from_hub (val)", params_map['pretrain_from_hub'])
+        live.log_param("retrain_user_model (val)", params_map['retrain_user_model'])
+
+        params_al = {
+            'gpu': '0',
+            'path_to_img_train': '/media/alex/DAtA4/Datasets/coco/my_dataset/train',
+            'path_to_labels_train': '/media/alex/DAtA4/Datasets/coco/for_al',
+            'path_to_img_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/val',
+            'path_to_labels_val': '/media/alex/DAtA4/Datasets/coco/my_dataset/labels_val/val.json',
+            'pretrain_from_hub': True,
+            'save_model': False,
+            'retrain_user_model': False,
+            'batch_unlabeled': -1,
+            'use_val_test_in_train': True,
+            'bbox_selection_policy': 'min',
+            'quantile_min': 0,
+            'quantile_max': 0.3,
+            'type_model': 'custom',
+            'num_epochs': 5
+        }
+
+        live.log_param("type_model (al)", params_al['type_model'])
+        live.log_param("quantile_min (al)", params_al['quantile_min'])
+        live.log_param("quantile_max (al)", params_al['quantile_max'])
+        live.log_param("batch_unlabeled (al)", params_al['batch_unlabeled'])
+        live.log_param("bbox_selection_policy (al)", params_al['bbox_selection_policy'])
+
+
         L = []
         path_to_img_train = '/media/alex/DAtA4/Datasets/coco/my_dataset/train'
         path_to_labels_train = '/media/alex/DAtA4/Datasets/coco/for_al'
@@ -102,13 +103,15 @@ if __name__ == '__main__':
                       path_to_out=os.path.join(path_to_labels_train, 'first.json'))
             a = []
             for kk in range(len(n_al)):
-                out = mAP(live)
+                out = mAP(params_map)
                 f, path_model = out[0], out[1]
                 a.append(f)
                 print('mAP', n_al[kk], f)
                 live.log_metric('mAP', f)
                 if kk != len(n_al) - 1:
-                    step = al(live, n_al[kk], path_model)
+                    params_al['path_model'] = path_model
+                    params_al['add'] = n_al[kk]
+                    step = al(params_al)
                     write_json(step,
                                kk,
                                path_to_out=path_to_labels_train,
